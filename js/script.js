b8a6f6e04db79b23744400304e8157468c6733ce
Joanna Szopa <joanna.aksamit5@wp.pl>
2022-01-18 23:02:26 +0100

    usuwanie klasy active

--- js/script.js
+++ js/script.js
@@ -3,25 +3,37 @@
     console.log('links:', links);
   });*/
 
-  function titleClickHandler (event) {
+function titleClickHandler(event) {
     console.log('Link was clicked!');
     console.log(event);
+
     /* remove class 'active' from all article links  */
 
-  /* add class 'active' to the clicked link */
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-  /* remove class 'active' from all articles */
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
 
-  /* get 'href' attribute from the clicked link */
+    /* add class 'active' to the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+    /* remove class 'active' from all articles */
 
-  /* add class 'active' to the correct article */
+    const activeArticles = document.querySelectorAll('.post active');
+
+    for (let activeArticle of activeArticles) {
+        activeLink.classList.remove('active');
+    }
+    /* get 'href' attribute from the clicked link */
+
+    /* find the correct article using the selector (value of 'href' attribute) */
+
+    /* add class 'active' to the correct article */
 
 }
-  
-  const links = document.querySelectorAll('.titles a');
-  
-  for(let link of links){
+
+const links = document.querySelectorAll('.titles a');
+
+for (let link of links) {
     link.addEventListener('click', titleClickHandler);
-  }
+}
