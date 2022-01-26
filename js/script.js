23e848ac1a1ec2fc77dd18c536d1146c35eb1406
Joanna Szopa <joanna.aksamit5@wp.pl>
2022-01-26 09:13:57 +0100

    Zmiany w scrypt

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const titleClickHandler= function(event) {
+  event.preventDefault();
   const clickedElement = this;
-    console.log('Link was clicked!');
-    
+  console.log('Link was clicked!');
 
     /*[DONE]remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -10,8 +10,8 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove("active");
 }
 
-    /*[IN PROGRESS] add class 'active' to the clicked link */
-    console.log('clickedElement: (with plus)' , clickedElement)
+    /*[DONE] add class 'active' to the clicked link */
+    console.log('clickedElement:' , clickedElement);
     clickedElement.classList.add('active');
 
 
@@ -22,10 +22,15 @@ for(let activeLink of activeLinks){
     }
 
     /* get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log( articleSelector);
 
     /* find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector('article');
+    console.log(targetArticle);
 
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
     }
     
     const links = document.querySelectorAll('.titles a');
